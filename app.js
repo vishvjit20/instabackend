ccdafb6ef4acbbc434697e08a1a8c815838b6dc2
vishwajeet <singhvishwajeet1998@gmail.com>
2021-09-26 23:22:44 +0530

    View other user's profile logic implemented

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { MONGOURI } = require("./keys");
 const User = require("./models/user");
 const Post = require("./models/posts");
 const postRoutes = require("./routes/post");
+const userRoutes = require("./routes/user");
 // const cors = require("cors");
 
 const PORT = 5000;
@@ -18,6 +19,7 @@ mongoose
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/post", postRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
